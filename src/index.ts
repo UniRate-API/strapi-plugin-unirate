959ca62cfc47ade53d0b217b4e8ec1775be6550e
rob-browncc <199503049+rob-browncc@users.noreply.github.com>
2026-07-01 06:18:07 +0000

    fix: export UniRatePluginConfig (was incorrectly named UniRateConfig)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default } from "./plugin.js";
 export { makePlugin } from "./plugin.js";
-export type { UniRateConfig } from "./plugin.js";
+export type { UniRatePluginConfig } from "./plugin.js";
 export {
   UniRateClient,
   UniRateError,
